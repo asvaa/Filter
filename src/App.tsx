6982eebb7eeb51c4bfd7f11bfddbc3272fcbcca1
Adam Mysaev <adam@Air-Adam.lan>
2024-03-07 14:47:52 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+type FilterType = "all" | "dollar" | "ruble"
+
+
 function App() {
+
+  const [money, setMoney] = useState([
+    { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+    { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+    { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+    { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+    { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+  ])
+  console.log((setMoney))
+  const [filter, setFilter] = useState<FilterType>("all")
+
+
+  let currentMoney = money;
+  if (filter === "dollar") {
+    currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === "Dollars")
+  }
+
+  if (filter === "ruble") {
+    currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === "RUBLS")
+  }
+
+  const oncklikFilterHandler = (nameButton: FilterType) => {
+    setFilter(nameButton)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {currentMoney.map((objectMoneyArr, index) => {
+          return (
+            <li key={index}>
+              <span>{objectMoneyArr.banknots}</span>
+              <span>{objectMoneyArr.value}</span>
+              <span>{objectMoneyArr.number}</span>
+            </li>
+          )
+        })}
+      </ul>
+      <div style={{ marginLeft: "35px" }}>
+        <button onClick={() => oncklikFilterHandler("all")}>all</button>
+        <button onClick={() => oncklikFilterHandler("ruble")}>rubles</button>
+        <button onClick={() => oncklikFilterHandler("dollar")}>dollars</button>
+      </div>
+    </>
+
+
   );
 }
 
